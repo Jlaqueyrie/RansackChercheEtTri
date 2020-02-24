674834427aa24692c866de94aac87fa930203089
Jlaqueyrie <33941177+Jlaqueyrie@users.noreply.github.com>
2020-02-24 10:09:11 +0100

    Add files via upload
    ajout d'un menu pour gérer multiple fichier de critère ransack.

--- RansackConv.py
+++ RansackConv.py
@@ -4,173 +4,260 @@ import csv
 import psutil
 from shutil import copy
 from os import getcwd, path, makedirs, remove
+from consolemenu import SelectionMenu
+import xlsxwriter
+import ctypes
+ctypes.windll.kernel32.SetConsoleTitleW("Recherche de logs")
+
+workbook = xlsxwriter.Workbook('Example2.xlsx')
+worksheet = workbook.add_worksheet()
+row = 0
+column = 0
 
 """Variable"""
-Liste = ""
-ListeSn = []
-ChaineRansack = ""
-SavedSearch = r"C:\Users\jlaqueyr\Documents\AgentRansack\CritereC62.srf"
-FichierResultat = getcwd() + r"\RansackResultat.csv"
-CheminRansack = r"C:\Program Files\Mythicsoft\Agent Ransack\AgentRansack.exe"
-ChaineRansack = ""
-RepRacine = r"C:\Users\jlaqueyr\Documents\ResultatRechercheRansack"
-Nom_Ransack = "AgentRansack.exe"
-MaxTentative = 3
-NbrTentative = 0
-Retour = 0
-NouveauxChemin = ""
-Sn = ""
+lst = ""
+nvxChn = ""
+sn = ""
+strRansack = ""
+lstsn = []
+lstFichierTrouve = []
+numCrit = 0
+maxTentative = 3
+retour = 0
+inputNok = True
+
+fichierResultat = getcwd() + r"\RansackResultat.csv"
+chnRansack = r"C:\Program Files\Mythicsoft\Agent Ransack\AgentRansack.exe"
+repRacine = r"C:\Users\jlaqueyr\Documents\ResultatRechercheRansack"
+
+savedSearch = [r"C:\Users\jlaqueyr\Documents\AgentRansack\CritereC62.srf",
+               r"C:\Users\jlaqueyr\Documents\AgentRansack\CritereEqUft.srf",
+               r"C:\Users\jlaqueyr\Documents\AgentRansack\CriteriaTest.srf"]
+lstProduit = ['VCM', 'EQUALIZER', 'test']
+
+PROCNAME = "AgentRansack.exe"
 
 """Fonction"""
 
 
-def CopyFichier(CheminInitial, RepFinal, NumSerie, MTentative):
+def copyFichier(chnInitial, repRacine, nomRecherche, numSerie, mTentative):
     """
     Entrée :
     Chemin initial = Chemin du fichier initial
+
     RepFinal = chemin destination final
-    NumSerie = Nom du dossier pour trier les fichiers de logs par
+    numSerie = Nom du dossier pour trier les fichiers de logs par
     num série
-    MTentative = nombre maximum de tentatives pour les copies de fichier
+    mTentative = nombre maximum de tentatives pour les copies de fichier
 
     Sortie:
-    Ret = Ce finit par 0 si il y a un erreur de copie, ce finit par 1 si pas de
+    ret = Ce finit par 0 si il y a un erreur de copie, ce finit par 1 si pas de
     d'erreur
     """
 
-    NomFichier = path.basename(CheminInitial)
+    nomFichier = path.basename(chnInitial)
 
-    NTentative = 0
-    Ret = 0
+    nTentative = 0
+    ret = 0
 
-    while NTentative <= MTentative or Ret==1:
+    while nTentative <= mTentative or ret == 1:
 
         try:
-                
-            NouveauxChemin = r"{}\{}\{}".format(RepRacine,
-                                                NumSerie,
-                                                NomFichier)                        
-            copy(CheminFichier, NouveauxChemin)
-            NTentative = MTentative
-            Ret = 1
+
+            nvxChn = r"{}\{}\{}".format(repRacine,
+                                                nomRecherche,
+                                                numSerie,
+                                                nomFichier)
+            #breakpoint()
+            copy(chnFichier, nvxChn)
+            nTentative = mTentative
+            ret = 1
             break
         except IOError:
             print("Impossible de copier le fichier. ")
-            NTentative = NTentative+1
-            Ret = 0
+            nTentative = nTentative + 1
+            ret = 0
 
-    return Ret
+    return ret, nvxChn
 
 
-def CreeDossier(RepRacine, NumSerie):
+def creeDossier(repRacine, nomRecherche, numSerie):
     """
     Entrée :
     RepFinal = chemin destination final
-    NumSerie = Nom du dossier pour trier les fichiers de logs par
+    numSerie = Nom du dossier pour trier les fichiers de logs par
     num série
     Sortie :
-    Ret = renvoie 1 si pas d'erreur lors d ela création de dossier et
+    ret = renvoie 1 si pas d'erreur lors d ela création de dossier et
     si une erreur
     """
-    Ret = 0
-    CheminDir = r"{}\{}".format(RepRacine, NumSerie)
+    ret = 0
+    chnDir = r"{}\{}\{}\\".format(repRacine, nomRecherche, numSerie)
 
-    if not path.exists(CheminDir):
+    if not path.exists(chnDir):
         try:
-            makedirs(CheminDir, 0o700)
-            Ret = 1
-        except :
+            makedirs(chnDir, 0o700)
+            ret = 1
+        except BaseException:
             print("une erreur c'est produite")
-            Ret = 0
+            ret = 0
     else:
-        Ret = 1
-    return Ret
+        ret = 1
+    return ret
 
 
-# main program
-Liste = pyperclip.paste()
-if Liste:
+def quelType(nomProduit, cheminFichier):
+    """
+    Entrée :
+        nomProduit : permet d'avoir différent moyen de reconnaissance des
+        fichier
+        cheminFichier : chemin menant au fichier à tester
+    Sortie:
+        resultat : dictionnaire contenant le numéros de série, type de fichier,
+        type testeur
+    """
+    resultat = {}
+    tTesteur = ''
 
-    if "\r\n" in Liste:
+    with open(cheminFichier, 'r') as objFichier:
+        contenuFichier = objFichier.readlines()
 
-        Liste = Liste.split("\r\n")
-        Liste = [x.strip() for x in Liste][:-1]
+    if nomProduit == 'vcm':
+        if 'L24' in contenuFichier[0]:
+            tFichier = 'RES'
+            if 'UFT' in contenuFichier[0]:
+                tTesteur = 'UFT'
+            elif 'TSDS' in contenuFichier[0]:
+                tTesteur = 'TSDS'
+            elif 'VCM_T' in contenuFichier[0]:
+                tTesteur = 'CADS'
+            else:
+                print('type de fichier inconnu')
+        else:
+            tFichier = 'LOG'
+            if 'Equipement Ref. :' in contenuFichier[3]:
+                tTesteur = 'UFT'
+            elif 'Starting process with SFC:' in contenuFichier[0]:
+                tTesteur = 'CADS'
+            elif 'TSDS' in contenuFichier[0]:
+                tTesteur = 'TSDS'
+            else:
+                print('fichier inconnu')
 
-        for Sn in Liste:
-            ChaineRansack = ChaineRansack + "*{}*;".format(Sn)
-            ListeSn.append(Sn)
+        resultat = {'typeFichier': tFichier,
+                    'typeTesteur': tTesteur}
+
     else:
+        print('test absent pour ce produit')
 
-        ChaineRansack = "*{}*".format(Liste)
-else:
+    return resultat
 
-    print("Rien à convertir")
-    input("Presser une touche pour sortir...")
+
+menu = SelectionMenu(lstProduit, "Select an option")
+menu.show()
+numCrit = menu.selected_option
+
+#option exit du menu
+if numCrit == len(lstProduit):
+
     exit()
 
-pyperclip.copy(ChaineRansack[:-1])
-
-cmd = "\"{0}\" {1} -o \"{3}\"  -ofc -f \"{2}\" ".format(
-    CheminRansack, SavedSearch, ChaineRansack, FichierResultat)
-
-print("Recherche des caractères suivant : {}".format(ChaineRansack))
-
-Retour = subprocess.run(cmd, shell=True)
-print("Recherche en cours")
-
-
-with open(FichierResultat) as Resultatrecherche:
-    ContenuFichier = csv.reader(Resultatrecherche, delimiter=',')
-    ContenuFichier = list(ContenuFichier)
-
-if len(ContenuFichier) > 1:
-
-    for Index, Ligne in enumerate(ContenuFichier):
-        if Index == 0:
-            """erreur de ransack à la première ligne :
-            il ajoute des 3 symboles"""
-            CheminFichier = Ligne[0][3:] + Ligne[1]
-        else:
-            CheminFichier = Ligne[0] + Ligne[1]
-
-        if path.isfile(CheminFichier):
-
-            for Sn in ListeSn:
-                RetourDossier = 0
-                RetourCopie = 0
-
-                if Sn in CheminFichier:
-                    #breakpoint()
-                    RetourDossier = CreeDossier(RepRacine, Sn)
-                    if RetourDossier:
-                        RetourCopie = CopyFichier(CheminFichier, RepRacine, Sn,
-                                                  MaxTentative)
-                        if RetourCopie:
-                            break
-                        else:
-                            print("Erreur lors de la copie fichier pour le SN :{}"
-                                  .format(Sn))
-                    else:
-                        print("Erreur lors de la création du \
-                              dossier" .format(Sn))
-                Sn = ""
-
-        else:
-            print("Le fichier {} n'existe \
-                  plus".format(path.basename(CheminFichier)))
-
 else:
-    print("Pas de résultat dans le fichier de recherche")
 
-print("Recherche fini")
-input("Presser une touche pour sortir...")
-remove(FichierResultat)
+    print('lancer une recherche pour \
+        les produits de type : {}'.format(lstProduit[numCrit]))
 
-# tester cette fonction : nom des paramètre en anglais
-for proc in psutil.process_iter(attrs=['pid', 'name']):
-    if Nom_Ransack in proc.info['name']:
-        proc.kill()
+    input('Presser une touche')
 
-ChaineRansack = ""
+    # MAIN PROGRAM
+    lst = pyperclip.paste()
 
-exit()
+    if lst:
+
+        if "\r\n" in lst:
+            lst = lst.split("\r\n")
+            lst = [x.strip() for x in lst][:-1]
+
+            for Sn in lst:
+                strRansack = strRansack + "*{}*;".format(Sn)
+                lstsn.append(Sn)
+        else:
+            strRansack = "*{}*".format(lst)
+
+    else:
+        print('aucun caractère à rechercher')
+        input("presser une touche pour sortir")
+        exit()
+
+    #Pyperclip.copy(chnransack[:-1])
+
+    cmd = "\"{0}\" {1} -o \"{3}\"  -ofc -f \"{2}\" ".format(chnRansack, savedSearch[numCrit], strRansack, fichierResultat)
+
+    print("recherche des caractères suivants : {}".format(strRansack))
+
+    retour = subprocess.run(cmd, shell=True)
+    print("recherche en cours patienter")
+
+    nomRecherche = ''
+    nomRecherche = input('entrée un nom pour la recherche')
+
+    with open(fichierResultat) as rsltRecherche:
+        contenuFichier = csv.reader(rsltRecherche, delimiter=',')
+        contenuFichier = list(contenuFichier)
+
+    if len(contenuFichier) >= 1:
+
+        for Index, Ligne in enumerate(contenuFichier):
+            infoFichier = {}
+            if Index == 0:
+                """erreur de ransack à la première ligne :
+                il ajoutes 3 symboles"""
+                chnFichier = Ligne[0][3:] + Ligne[1]
+            else:
+                chnFichier = Ligne[0] + Ligne[1]
+
+            if path.isfile(chnFichier):
+
+                for sn in lstsn:
+                    retourDossier = 0
+                    retourCopie = 0
+
+                    if sn in chnFichier:
+                        # breakpoint()
+                        retourDossier = creeDossier(repRacine, nomRecherche, sn)
+                        if retourDossier:
+                            retourCopie, cheminFcihier = copyFichier(
+                                chnFichier, repRacine, nomRecherche, sn, maxTentative)
+                            if retourCopie:
+                                infoFichier = quelType('vcm', chnFichier)
+                                infoFichier['numSerie'] = sn
+                                lstFichierTrouve.append(infoFichier)
+                                break
+                            else:
+                                print(
+                                    "Erreur lors de la copie fichier \
+                                    pour le SN :{}" .format(sn))
+                        else:
+                            print("Erreur lors de la création du dossier" .format(sn))
+
+            else:
+                print("Le fichier {} n'existe plus".format(chnFichier))
+
+    else:
+        print("Pas de résultat dans le fichier de recherche")
+
+    print("Recherche fini")
+    input("Presser une touche pour sortir...")
+    remove(fichierResultat)
+
+    # tester cette fonction : nom des paramètre en anglais
+
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == PROCNAME:
+            proc.kill()
+
+    strRansack = ""
+    print(lstFichierTrouve)
+    input()
+    #exit()
